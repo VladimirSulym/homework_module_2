3d47950e45e5215fe78588d012e4338c4cf8b929
Vladimir Sulym <v.sulym@gmail.com>
2024-08-18 16:45:46 +0300

    a test has been created for the get_mask_account function

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,6 +1,6 @@
 import pytest
 
-from src.masks import get_mask_card_number
+from src.masks import get_mask_card_number, get_mask_account
 
 
 @pytest.mark.parametrize(
@@ -11,9 +11,30 @@ from src.masks import get_mask_card_number
         ("7000792289606361", "7000 79** **** 6361"),
         ("70007289606361", None),
         ("7158300734726758", "7158 30** **** 6758"),
+        ("71583ab734726758", None),
         ("", None),
         (None, None),
+        (False, None),
+        (True, None),
     ],
 )
 def test_get_mask_card_number(number, expected):
     assert get_mask_card_number(number) == expected
+
+
+@pytest.mark.parametrize(
+    "number, expected",
+    [
+        (64686473678894779589, "**9589"),
+        (111111111, None),
+        ("35383033474447895560", "**5560"),
+        ("70007289606361", None),
+        ("7000ad89606361", None),
+        ("", None),
+        (None, None),
+        (False, None),
+        (True, None),
+    ],
+)
+def test_get_mask_account(number, expected):
+    assert get_mask_account(number) == expected
