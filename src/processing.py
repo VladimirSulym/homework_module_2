3be0cf078e1882bf1c1e86fc47ef460e0bb9869d
Vladimir Sulym <v.sulym@gmail.com>
2024-08-18 08:23:55 +0300

    creating the sort_by_date function

--- src/processing.py
+++ src/processing.py
@@ -3,3 +3,7 @@ from typing import Iterable, Union
 
 def filter_by_state(my_list: Iterable[list], state: Union[str] = "EXECUTED") -> Iterable[list]:
     return [i for i in my_list if i["state"] == state]
+
+
+def sort_by_date(my_list: Iterable[list], sort_descending: Union[bool] = 'True') -> Iterable[list]:
+    return sorted(my_list, key=lambda x: x["date"], reverse=sort_descending)
