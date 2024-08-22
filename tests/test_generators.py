b2cb7d89b1e73b873e2959983e8b3b58045079bc
Vladimir Sulym <v.sulym@gmail.com>
2024-08-22 22:53:36 +0300

    the test_generators modul has been created

---
+++ tests/test_generators.py
@@ -0,0 +1,7 @@
+from src.generators import filter_by_currency, transaction_descriptions, card_number_generator
+import pytest
+
+
+@pytest.mark.parametrize()
+def test_filter_by_currency():
+    pass
