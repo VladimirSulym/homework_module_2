2d5a5d2bf1d735bbf503346a0fdd8b37f5b43e84
Vladimir Sulym <v.sulym@gmail.com>
2024-08-23 21:30:40 +0300

    All tests for the transaction_descriptions function have been completed

--- tests/test_generators.py
+++ tests/test_generators.py
@@ -66,6 +66,10 @@ def test_filter_by_currency_2(my_lis_transactions):
         assert str(exc_info.value) == "StopIteration"
 
     with pytest.raises(StopIteration) as exc_info:
+        next(filter_by_currency(my_lis_transactions, "EUR"))
+        assert str(exc_info.value) == "StopIteration"
+
+    with pytest.raises(StopIteration) as exc_info:
         next(filter_by_currency([], None))
         assert str(exc_info.value) == "StopIteration"
 
@@ -83,6 +87,73 @@ def test_filter_by_currency_2(my_lis_transactions):
 
     with pytest.raises(StopIteration) as exc_info:
         next(filter_by_currency())
+        assert str(exc_info.value) == "StopIteration"
+
+    with pytest.raises(StopIteration) as exc_info:
+        next(
+            filter_by_currency(
+                [
+                    {
+                        "id": 873106923,
+                        "state": "EXECUTED",
+                        "date": "2019-03-23T01:09:46.296404",
+                        "operationAmount": {"amount": "43318.34"},
+                        "description": "Перевод со счета на счет",
+                        "from": "Счет 44812258784861134719",
+                        "to": "Счет 74489636417521191160",
+                    }
+                ],
+                "RUB",
+            )
+        )
+        assert str(exc_info.value) == "StopIteration"
+
+
+def test_transaction_descriptions_1(my_lis_transactions):
+    generator = transaction_descriptions(my_lis_transactions)
+    assert next(generator) == "Перевод организации"
+    assert next(generator) == "Перевод со счета на счет"
+    assert next(generator) == "Перевод со счета на счет"
+    assert next(generator) == "Перевод с карты на карту"
+    assert next(generator) == "Перевод организации"
+
+    with pytest.raises(StopIteration) as exc_info:
+        next(generator)
+        assert str(exc_info.value) == "StopIteration"
+
+
+def test_transaction_descriptions_2():
+    with pytest.raises(StopIteration) as exc_info:
+        next(transaction_descriptions([]))
+        assert str(exc_info.value) == "StopIteration"
+
+    with pytest.raises(StopIteration) as exc_info:
+        next(transaction_descriptions())
+        assert str(exc_info.value) == "StopIteration"
+
+    with pytest.raises(StopIteration) as exc_info:
+        next(transaction_descriptions(None))
+        assert str(exc_info.value) == "StopIteration"
+
+    with pytest.raises(StopIteration) as exc_info:
+        next(transaction_descriptions(123))
+        assert str(exc_info.value) == "StopIteration"
+
+    with pytest.raises(StopIteration) as exc_info:
+        next(
+            transaction_descriptions(
+                [
+                    {
+                        "id": 142264268,
+                        "state": "EXECUTED",
+                        "date": "2019-04-04T23:20:05.206878",
+                        "operationAmount": {"amount": "79114.93", "currency": {"name": "USD", "code": "USD"}},
+                        "from": "Счет 19708645243227258542",
+                        "to": "Счет 75651667383060284188",
+                    }
+                ]
+            )
+        )
         assert str(exc_info.value) == "StopIteration"
 
 
