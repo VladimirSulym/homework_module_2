4d22521bd29961d2f51bdb10722770e22d0f14e1
Vladimir Sulym <v.sulym@gmail.com>
2024-08-25 23:43:52 +0300

    all PEP 8 checks have been passed

--- src/external_api.py
+++ src/external_api.py
@@ -1,20 +1,20 @@
-import requests
 import os
-import json
+
+import requests
 from dotenv import load_dotenv
 
 load_dotenv()
+
+
 def currency_conversion(currency, amount):
-  url = "https://api.apilayer.com/exchangerates_data/convert"
-  payload = {
-    'to': 'RUB',
-    'from': currency,
-    'amount': str(amount),
-  }
-  headers= {
-    "apikey": os.getenv('API_KEY')
-  }
-  response = requests.get(url, headers = headers, params=payload)
-  # status_code = response.status_code
-  result = response.json()
-  return round(result['result'], 2)
+    url = "https://api.apilayer.com/exchangerates_data/convert"
+    payload = {
+        "to": "RUB",
+        "from": currency,
+        "amount": str(amount),
+    }
+    headers = {"apikey": os.getenv("API_KEY")}
+    response = requests.get(url, headers=headers, params=payload)
+    # status_code = response.status_code
+    result = response.json()
+    return round(result["result"], 2)
--- src/utils.py
+++ src/utils.py
@@ -1,23 +1,29 @@
-import os.path
 import json
+import os.path
+
 from config import DATA_PATH
 from src.external_api import currency_conversion
 
+
 def get_data_json(path):
     try:
-        with open(path, 'r') as f:
+        with open(path, "r") as f:
             return json.load(f)
     except Exception:
         return []
 
-base_data = get_data_json(os.path.join(DATA_PATH, 'operations.json'))
+
+base_data = get_data_json(os.path.join(DATA_PATH, "operations.json"))
+
 
 def get_amount_transactions_in_rub(transaction):
-    if transaction['operationAmount']['currency']['code'] != 'RUB':
-        return currency_conversion(transaction['operationAmount']['currency']['code'],
-                            transaction['operationAmount']['amount'])
+    if transaction["operationAmount"]["currency"]["code"] != "RUB":
+        return currency_conversion(
+            transaction["operationAmount"]["currency"]["code"], transaction["operationAmount"]["amount"]
+        )
     else:
-        return transaction['operationAmount']['amount']
+        return transaction["operationAmount"]["amount"]
 
-for i in range(10, 15):
-    print(get_amount_transactions_in_rub(base_data[i]))
+
+# for i in range(10, 15):
+#     print(get_amount_transactions_in_rub(base_data[i]))
