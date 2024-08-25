ac7495b2f19317555775e183f3a4d4ccb6089f33
Vladimir Sulym <v.sulym@gmail.com>
2024-08-25 23:36:59 +0300

    get_amount_transactions_in_rub and currency_conversion functions was created

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,7 @@
 import os.path
 import json
 from config import DATA_PATH
+from src.external_api import currency_conversion
 
 def get_data_json(path):
     try:
@@ -9,4 +10,14 @@ def get_data_json(path):
     except Exception:
         return []
 
-print(get_data_json(os.path.join(DATA_PATH, 'operations.json')))
+base_data = get_data_json(os.path.join(DATA_PATH, 'operations.json'))
+
+def get_amount_transactions_in_rub(transaction):
+    if transaction['operationAmount']['currency']['code'] != 'RUB':
+        return currency_conversion(transaction['operationAmount']['currency']['code'],
+                            transaction['operationAmount']['amount'])
+    else:
+        return transaction['operationAmount']['amount']
+
+for i in range(10, 15):
+    print(get_amount_transactions_in_rub(base_data[i]))
