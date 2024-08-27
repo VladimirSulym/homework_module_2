6f7751fdc0dd11b489b376c1cdf932b0978933b1
Vladimir Sulym <v.sulym@gmail.com>
2024-08-27 22:53:43 +0300

    the test for the currency_conversion function from the external_api module has been upgraded

--- tests/test_external_api.py
+++ tests/test_external_api.py
@@ -1,18 +1,17 @@
 from src.external_api import currency_conversion
-import os
-from dotenv import load_dotenv
+
 from unittest.mock import patch
 
-load_dotenv()
 
-
+@patch("os.getenv")
 @patch("requests.get")
-def test_currency_conversion(mock_get):
+def test_currency_conversion(mock_get, mock_getenv):
     mock_get.return_value.json.return_value = {"result": 2.00}
+    mock_getenv.return_value = "123"
     assert currency_conversion("USD", 2) == 2.00
     mock_get.assert_called_once_with(
         "https://api.apilayer.com/exchangerates_data/convert",
-        headers={"apikey": os.getenv("API_KEY")},
+        headers={"apikey": "123"},
         params={
             "to": "RUB",
             "from": "USD",
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,2 +1,4 @@
 from src.utils import get_data_json, get_amount_transactions_in_rub
 
+# def test_get_data_json():
+#     assert get_data_json() ==
