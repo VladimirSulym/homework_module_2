84ec65ad1663be089fcdcf6ccbdd91e4286884ad
Vladimir Sulym <v.sulym@gmail.com>
2024-09-02 21:09:07 +0300

    all PEP 8 checks have been passed

--- src/masks.py
+++ src/masks.py
@@ -4,48 +4,52 @@ import logging
 import os
 
 
-logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)s - Modul: %(filename)s - LogName: %(name)s => %(levelname)s: %(message)s',
-                    filename=os.path.join(LOGS_PATH, 'application.log'),
-                    filemode='a')
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s - Modul: %(filename)s - LogName: %(name)s => %(levelname)s: %(message)s",
+    filename=os.path.join(LOGS_PATH, "application.log"),
+    filemode="a",
+)
 
-masks_logger = logging.getLogger('app.masks')
-masks_logger.info('Прямой запуск модуля masks.py')
+masks_logger = logging.getLogger("app.masks")
+masks_logger.info("Прямой запуск модуля masks.py")
+
 
 def get_mask_card_number(card_number: Union[str, int]) -> Optional[str]:
     """Функция принимает на вход номер карты в виде строки или числа и возвращает ее маску."""
-    masks_logger.info('Запущена функция get_mask_card_number')
+    masks_logger.info("Запущена функция get_mask_card_number")
     if card_number and len(str(card_number)) == 16 and str(card_number).isdigit():
-        masks_logger.info('Получен правильный номер карты')
+        masks_logger.info("Получен правильный номер карты")
         str_to_list = list(str(card_number))
         # заменяем цифры с 6 по 11 на *
         for i in [6, 7, 8, 9, 10, 11]:
-            masks_logger.debug(f'Замена {i} цифры в номере карты')
+            masks_logger.debug(f"Замена {i} цифры в номере карты")
             str_to_list[i] = "*"
         # вставляем необходимые пробелы в позиции 4, 9, 14
         for i in [4, 9, 14]:
-            masks_logger.debug(f'Вставка пробела в позицию {i}')
+            masks_logger.debug(f"Вставка пробела в позицию {i}")
             str_to_list.insert(i, " ")
-        masks_logger.info('Маска для карты создана')
-        masks_logger.info('Функция get_mask_card_number завершила свою работу положительно')
+        masks_logger.info("Маска для карты создана")
+        masks_logger.info("Функция get_mask_card_number завершила свою работу положительно")
         return "".join(str_to_list)
     else:
-        masks_logger.error('Получен не валидный номер карты')
-        masks_logger.warning('Функция get_mask_card_number завершила работу с ошибкой')
+        masks_logger.error("Получен не валидный номер карты")
+        masks_logger.warning("Функция get_mask_card_number завершила работу с ошибкой")
         return None
 
 
 def get_mask_account(account_number: Union[str, int]) -> Optional[str]:
     """Функция принимает на вход номер счета и возвращает его маску."""
-    masks_logger.info('Запущена функция get_mask_account')
+    masks_logger.info("Запущена функция get_mask_account")
     if account_number and len(str(account_number)) == 20 and str(account_number).isdigit():
-        masks_logger.info('Получен правильный номер счета')
-        masks_logger.info('Функция get_mask_account завершила свою работу положительно')
+        masks_logger.info("Получен правильный номер счета")
+        masks_logger.info("Функция get_mask_account завершила свою работу положительно")
         return "**" + str(account_number)[-4::1]
     else:
-        masks_logger.error('Получен не валидный номер счета')
-        masks_logger.warning('Функция get_mask_account завершила свою работу с ошибкой')
+        masks_logger.error("Получен не валидный номер счета")
+        masks_logger.warning("Функция get_mask_account завершила свою работу с ошибкой")
         return None
+
 
 # if __name__ == '__main__':
 #     get_mask_card_number(7000792289602361)
--- src/utils.py
+++ src/utils.py
@@ -4,49 +4,51 @@ import os
 from config import LOGS_PATH
 import logging
 
-from config import DATA_PATH
 from src.external_api import currency_conversion
 
-logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)s - Modul: %(filename)s - LogName: %(name)s => %(levelname)s: %(message)s',
-                    filename=os.path.join(LOGS_PATH, 'application.log'),
-                    filemode='a')
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s - Modul: %(filename)s - LogName: %(name)s => %(levelname)s: %(message)s",
+    filename=os.path.join(LOGS_PATH, "application.log"),
+    filemode="a",
+)
 
-utils_logger = logging.getLogger('app.utils')
-utils_logger.info('Прямой запуск модуля utils.py')
+utils_logger = logging.getLogger("app.utils")
+utils_logger.info("Прямой запуск модуля utils.py")
+
 
 def get_data_json(my_path):
     """Функция принимает на вход путь до JSON-файла и возвращает список словарей с данными о финансовых транзакциях"""
-    utils_logger.info('Запущена функция get_data_json')
+    utils_logger.info("Запущена функция get_data_json")
     try:
         with open(my_path, "r") as f:
-            utils_logger.debug(f'Файл {os.path.basename(my_path)} открыт в режиме чтения')
-            utils_logger.debug(f'Информация из файла {os.path.basename(my_path)} преобразована в json')
-            utils_logger.info('Функция get_data_json завершила работу положительно')
+            utils_logger.debug(f"Файл {os.path.basename(my_path)} открыт в режиме чтения")
+            utils_logger.debug(f"Информация из файла {os.path.basename(my_path)} преобразована в json")
+            utils_logger.info("Функция get_data_json завершила работу положительно")
             return json.load(f)
     except Exception as e:
-        utils_logger.error(f'Ошибка в функции get_data_json:\n{e}', exc_info=True)
-        utils_logger.warning('Функция get_data_json завершила работу с ошибкой')
+        utils_logger.error(f"Ошибка в функции get_data_json:\n{e}", exc_info=True)
+        utils_logger.warning("Функция get_data_json завершила работу с ошибкой")
         return []
 
 
 def get_amount_transactions_in_rub(transaction):
     """Функция принимает на вход транзакцию и возвращает сумму транзакции в рублях"""
-    utils_logger.info('Запущена функция get_amount_transactions_in_rub')
+    utils_logger.info("Запущена функция get_amount_transactions_in_rub")
     try:
         if transaction["operationAmount"]["currency"]["code"] != "RUB":
-            utils_logger.debug('Выбранная валюта НЕ рубли')
-            utils_logger.info('Функция get_amount_transactions_in_rub завершила работу положительно')
+            utils_logger.debug("Выбранная валюта НЕ рубли")
+            utils_logger.info("Функция get_amount_transactions_in_rub завершила работу положительно")
             return currency_conversion(
                 transaction["operationAmount"]["currency"]["code"], transaction["operationAmount"]["amount"]
             )
         else:
-            utils_logger.debug('Выбранная валюта рубли')
-            utils_logger.info('Функция get_amount_transactions_in_rub завершила работу положительно')
+            utils_logger.debug("Выбранная валюта рубли")
+            utils_logger.info("Функция get_amount_transactions_in_rub завершила работу положительно")
             return float(transaction["operationAmount"]["amount"])
     except Exception as e:
-        utils_logger.error(f'Ошибка в функции get_amount_transactions_in_rub:\n{e}', exc_info=True)
-        utils_logger.warning('Функция get_amount_transactions_in_rub завершила работу с ошибкой')
+        utils_logger.error(f"Ошибка в функции get_amount_transactions_in_rub:\n{e}", exc_info=True)
+        utils_logger.warning("Функция get_amount_transactions_in_rub завершила работу с ошибкой")
         return None
 
 
