b61ddc1145594933e47380312d452708fc6e87d0
Vladimir Sulym <v.sulym@gmail.com>
2024-09-04 09:59:46 +0300

    added docstring for all functions

--- src/read_csv_xlsx.py
+++ src/read_csv_xlsx.py
@@ -1,12 +1,14 @@
 import pandas
 
 
-def convert_csv_to_list(path):
+def convert_csv_to_list(path: str) -> list:
+    """Функция для считывания финансовых операций из CSV файла и выдает список словарей с транзакциями."""
     df = pandas.read_csv(path, delimiter=";")
     return df.to_dict("records")
 
 
-def convert_xlsx_to_list(path):
+def convert_xlsx_to_list(path: str) -> list:
+    """Функция для считывания финансовых операций из Excel файла и выдает список словарей с транзакциями."""
     df = pandas.read_excel(path, decimal=";")
     return df.to_dict("records")
 
