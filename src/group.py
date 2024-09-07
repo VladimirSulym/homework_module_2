746aaebe5a720e1374233b2e05e14458d3964cb1
Vladimir Sulym <v.sulym@gmail.com>
2024-09-07 13:02:29 +0300

    The group.py module has been created. The group_agg function has been created

---
+++ src/group.py
@@ -0,0 +1,30 @@
+import os
+from collections import Counter
+
+from config import DATA_PATH
+from src.read_csv_xlsx import convert_csv_to_list
+
+
+def group_agg(dict_list, categories_list):
+    """Функция принимает список словарей с данными о банковских операциях и список категорий операций,
+    а возвращать словарь, в котором ключи — это названия категорий, а значения — это количество операций в каждой категории.
+    """
+    filter_dict = list(filter(lambda x: x["description"] in categories_list, dict_list))
+    data_counting = []
+    for i in filter_dict:
+        data_counting.append(i["description"])
+    counted = Counter(data_counting)
+    return dict(counted)
+
+
+print(
+    group_agg(
+        convert_csv_to_list(os.path.join(DATA_PATH, "transactions.csv")),
+        [
+            "Перевод организации",
+            "Перевод со счета на счет",
+            "Перевод с карты на карту",
+            "Открытие вклада",
+        ],
+    )
+)
--- src/search.py
+++ src/search.py
@@ -5,15 +5,19 @@ from collections import Counter
 from config import DATA_PATH
 from src.read_csv_xlsx import convert_csv_to_list
 
+
 def transaction_search(dicts_trans: list, search_str: str) -> list:
-    """ Функция принимает список словарей с данными о банковских операциях и строку поиска,
+    """Функция принимает список словарей с данными о банковских операциях и строку поиска,
     а возвращать список словарей, у которых есть данные в записях совпадающие с данной строкой. Функция осуществляет
-    поиск по всем полям и возвращает максимально подходящие варианты ответа в зависимости от степени совпадения данных"""
-    search_str_list = re.findall(r"[^\d\W][a-zA-Zа-яА-Я\s]+[^\W\d]|[a-zA-Zа-яА-Я]", search_str, flags=re.IGNORECASE | re.DOTALL)
+    поиск по всем полям и возвращает максимально подходящие варианты ответа в зависимости от степени совпадения данных
+    """
+    search_str_list = re.findall(
+        r"[^\d\W][a-zA-Zа-яА-Я\s]+[^\W\d]|[a-zA-Zа-яА-Я]", search_str, flags=re.IGNORECASE | re.DOTALL
+    )
     search_int_list = re.findall(r"\d+\W\d+\W\d+|\d+", search_str, flags=re.IGNORECASE | re.DOTALL)
     # print(search_str_list)
     # print(search_int_list)
-    result =[]
+    result = []
     for dict_trans in dicts_trans:
         for key, value in dict_trans.items():
             if search_int_list:
@@ -42,7 +46,9 @@ def transaction_search(dicts_trans: list, search_str: str) -> list:
     else:
         return result
 
+
 # transaction_search(convert_csv_to_list(os.path.join(DATA_PATH, "transactions.csv")),"Euro")
 
-for i in transaction_search(convert_csv_to_list(os.path.join(DATA_PATH, "transactions.csv")),"ОткрыТие вКЛада, EUR, 79277383088424042634")[:]:
-    print(i)
+# for i in transaction_search(convert_csv_to_list(os.path.join(DATA_PATH, "transactions.csv")),
+#                             "ОткрыТие вКЛада, EUR, 79277383088424042634")[:]:
+#     print(i)
